c763065c208a4a7809fab4b853f5f0a397cd64cc
Nikhil Chopde <chopdeniks@gmail.com>
2021-03-07 20:55:03 +0530

    Update starter_wp.php

--- starter_wp.php
+++ starter_wp.php
@@ -16,6 +16,9 @@ if ( ! defined( 'ABSPATH' ) ) {
 	die;
 }
 
+add_filter('show_admin_bar', '__return_false');
+
+add_action( 'wp_dashboard_setup', 'starter_wp_remove_all_dashboard_metaboxes' );
 function starter_wp_remove_all_dashboard_metaboxes() {
     // Remove Welcome panel
     remove_action( 'welcome_panel', 'wp_welcome_panel' );
@@ -25,5 +28,38 @@ function starter_wp_remove_all_dashboard_metaboxes() {
     remove_meta_box( 'health_check_status', 'dashboard', 'normal' );
     remove_meta_box( 'dashboard_right_now', 'dashboard', 'normal' );
     remove_meta_box( 'dashboard_activity', 'dashboard', 'normal');
+	remove_meta_box( 'dashboard_site_health', 'dashboard', 'normal');
 }
-add_action( 'wp_dashboard_setup', 'starter_wp_remove_all_dashboard_metaboxes' );
+
+add_action('admin_head', 'starter_wp_admin_styles');
+function starter_wp_admin_styles() {
+?>
+<style>
+#adminmenu li.wp-not-current-submenu.wp-menu-separator {
+    display: none!important;
+}	
+</style>
+<?php	
+}
+
+add_action('admin_menu','starter_wp_admin_footer_version');
+function starter_wp_admin_footer_version() {
+    remove_filter( 'update_footer', 'core_update_footer' );
+}
+
+add_filter('admin_footer_text', 'starter_wp_admin_footer_change');
+function starter_wp_admin_footer_change () {
+  echo '<i>Thank you!</i>';
+}
+
+add_filter('contextual_help_list','starter_wp_contextual_help_remove');
+function starter_wp_contextual_help_remove(){
+    global $current_screen;
+    $current_screen->remove_help_tabs();
+}
+
+add_action( 'wp_before_admin_bar_render', 'starter_wp_admin_bar_remove_logo', 0 );
+function starter_wp_admin_bar_remove_logo() {
+    global $wp_admin_bar;
+    $wp_admin_bar->remove_menu( 'wp-logo' );
+}
