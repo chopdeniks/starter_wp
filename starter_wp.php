f363f7c857f8630ebe287172c4604b89a8528074
Nikhil Chopde <chopdeniks@gmail.com>
2021-03-07 19:30:38 +0530

    Update starter_wp.php

--- starter_wp.php
+++ starter_wp.php
@@ -1,1 +1,18 @@
+<?php 
+/**
+ * Plugin Name:       Starter Plugin
+ * Plugin URI:        https://github.com/chopdeniks/starter_wp
+ * Description:       This is starter plugin with minimal tasks automated by just installing. No setting required. 
+ * Version:           1.0.0
+ * Author:            Nikhil Chopde
+ * Text Domain:       github-updater
+ * GitHub Plugin URI: https://github.com/chopdeniks/starter_wp
+ */
+
+/*
+ * Exit if called directly.
+ */
+if ( ! defined( 'ABSPATH' ) ) {
+	die;
+}
 
