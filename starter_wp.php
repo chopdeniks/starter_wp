8527a15b023a32e6e620fc1f1dd607e87dd72b4a
Nikhil Chopde <chopdeniks@gmail.com>
2021-03-07 19:40:11 +0530

    Update starter_wp.php

--- starter_wp.php
+++ starter_wp.php
@@ -16,4 +16,14 @@ if ( ! defined( 'ABSPATH' ) ) {
 	die;
 }
 
-remove_action('welcome_panel', 'wp_welcome_panel');
+function starter_wp_remove_all_dashboard_metaboxes() {
+    // Remove Welcome panel
+    remove_action( 'welcome_panel', 'wp_welcome_panel' );
+    // Remove the rest of the dashboard widgets
+    remove_meta_box( 'dashboard_primary', 'dashboard', 'side' );
+    remove_meta_box( 'dashboard_quick_press', 'dashboard', 'side' );
+    remove_meta_box( 'health_check_status', 'dashboard', 'normal' );
+    remove_meta_box( 'dashboard_right_now', 'dashboard', 'normal' );
+    remove_meta_box( 'dashboard_activity', 'dashboard', 'normal');
+}
+add_action( 'wp_dashboard_setup', 'starter_wp_remove_all_dashboard_metaboxes' );
