f3d96fb9bc697c9fc8a8644df86fef0f37b657ff
Nikhil Chopde <chopdeniks@gmail.com>
2024-03-19 09:12:02 +0530

    Update init.php

--- inc/init.php
+++ inc/init.php
@@ -8,12 +8,8 @@
 // Exit if accessed directly
 if ( ! defined( 'ABSPATH' ) ) exit;
 
-// include tgmpa requirement
-// Use the filter 'swp_enable_TGMPA' to disable inclusion of TGMPA in your theme's functions.php.
-// add_filter('swp_enable_TGMPA', '__return_false');
-if (apply_filters('swp_enable_TGMPA', true)) {
-    require_once dirname(__FILE__) . '/required_plugins.php';
-}
+//include tgmpa requirement
+require_once dirname( __FILE__ ) . '/required_plugins.php';
 
 //include core-wp files
 require_once dirname( __FILE__ ) . '/core-wp/core_wp.php';
