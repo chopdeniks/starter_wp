eeea3b9ac53e4d45d1c3eb35f6d89020a93487b7
Nikhil Chopde <chopdeniks@gmail.com>
2024-09-20 12:25:32 +0530

    Update core_wp.php
    Disable Elementor Tracker Notice and Tracking

--- inc/core-wp/core_wp.php
+++ inc/core-wp/core_wp.php
@@ -15,3 +15,17 @@ add_filter('pre_option_moderation_notify', '__return_zero');
 
 // Disable admin email verification
 add_filter('admin_email_check_interval', '__return_false');
+
+// Disable Elementor Tracker Notice and Tracking
+add_action( 'init', function() {
+    // Set the tracker notice as already dismissed
+    update_option( 'elementor_tracker_notice', '1' );
+    
+    // Disable tracking by preventing the tracker from being allowed
+    if ( class_exists( 'Elementor\Plugin' ) ) {
+        remove_action( 'admin_notices', [ \Elementor\Plugin::instance()->admin, 'notice_tracker' ] );
+
+        // Optional: Automatically opt-out of tracking
+        update_option( 'elementor_allow_tracking', 'no' );
+    }
+});
